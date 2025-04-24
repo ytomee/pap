79fdd2dfe8aef90910dcc9236d6d59d8f74ded52
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-04-24 09:22:03 +0100

    main responsive

--- JetHire/JetHire/pages/_app.js
+++ JetHire/JetHire/pages/_app.js
@@ -13,6 +13,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         <>  
             <Head>
                 <title>Jet Hire</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Head>
             <SessionProvider session={session}>
                 <Component {...pageProps} />
