24bbeb0a1e24a6fb75275c794da13f05ae9da5e6
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-01 20:52:26 +0100

    checkbox

--- JetHire/Dashboard/src/components/auth/SignInForm.tsx
+++ JetHire/Dashboard/src/components/auth/SignInForm.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Checkbox from "@/components/form/input/Checkbox";
 import Input from "@/components/form/input/InputField";
 import Label from "@/components/form/Label";
 import Button from "@/components/ui/button/Button";
