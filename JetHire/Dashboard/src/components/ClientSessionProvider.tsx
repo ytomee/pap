cb03a21b5d285cd8e6d5ce547210099b64f53306
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-01 22:07:26 +0100

    -apps

--- JetHire/Dashboard/src/components/ClientSessionProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-const ClientSessionProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default ClientSessionProvider;
